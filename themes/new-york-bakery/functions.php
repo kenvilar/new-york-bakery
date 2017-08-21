30a9d42546d17fcea18789552fd03ffff9e8a547
admin <kenvilar@gmail.com>
2017-08-22 02:40:25 +0800

    Register the new custom.js

--- themes/new-york-bakery/functions.php
+++ themes/new-york-bakery/functions.php
@@ -134,6 +134,9 @@ function new_york_bakery_scripts() {
 	wp_enqueue_script( 'new-york-bakery-skip-link-focus-fix',
 		get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(),
 		get_bloginfo( 'version' ), true );
+	wp_enqueue_script( 'custom-js',
+		get_template_directory_uri() . '/assets/js/custom.js', array( 'jquery' ),
+		get_bloginfo( 'version' ), true );
 	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
