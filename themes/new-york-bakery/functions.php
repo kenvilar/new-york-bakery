3cf98066f85b3615ce4a177f8d94985e30e592a9
admin <kenvilar@gmail.com>
2017-08-22 02:18:51 +0800

    Reformat functions.php

--- themes/new-york-bakery/functions.php
+++ themes/new-york-bakery/functions.php
@@ -2,7 +2,7 @@
 /**
  * New York Bakery functions and definitions
  *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ * @link    https://developer.wordpress.org/themes/basics/theme-functions/
  *
  * @package New_York_Bakery
  */
@@ -23,10 +23,10 @@ if ( ! function_exists( 'new_york_bakery_setup' ) ) :
 		 * to change 'new-york-bakery' to the name of your theme in all the template files.
 		 */
 		load_theme_textdomain( 'new-york-bakery', get_template_directory() . '/languages' );
-
+		
 		// Add default posts and comments RSS feed links to head.
 		add_theme_support( 'automatic-feed-links' );
-
+		
 		/*
 		 * Let WordPress manage the document title.
 		 * By adding theme support, we declare that this theme does not use a
@@ -34,19 +34,19 @@ if ( ! function_exists( 'new_york_bakery_setup' ) ) :
 		 * provide it for us.
 		 */
 		add_theme_support( 'title-tag' );
-
+		
 		/*
 		 * Enable support for Post Thumbnails on posts and pages.
 		 *
 		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		 */
 		add_theme_support( 'post-thumbnails' );
-
+		
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
 			'menu-1' => esc_html__( 'Primary', 'new-york-bakery' ),
 		) );
-
+		
 		/*
 		 * Switch default core markup for search form, comment form, and comments
 		 * to output valid HTML5.
@@ -58,16 +58,16 @@ if ( ! function_exists( 'new_york_bakery_setup' ) ) :
 			'gallery',
 			'caption',
 		) );
-
+		
 		// Set up the WordPress core custom background feature.
 		add_theme_support( 'custom-background', apply_filters( 'new_york_bakery_custom_background_args', array(
 			'default-color' => 'ffffff',
 			'default-image' => '',
 		) ) );
-
+		
 		// Add theme support for selective refresh for widgets.
 		add_theme_support( 'customize-selective-refresh-widgets' );
-
+		
 		/**
 		 * Add support for core custom logo.
 		 *
@@ -93,6 +93,7 @@ add_action( 'after_setup_theme', 'new_york_bakery_setup' );
 function new_york_bakery_content_width() {
 	$GLOBALS['content_width'] = apply_filters( 'new_york_bakery_content_width', 640 );
 }
+
 add_action( 'after_setup_theme', 'new_york_bakery_content_width', 0 );
 
 /**
@@ -111,6 +112,7 @@ function new_york_bakery_widgets_init() {
 		'after_title'   => '</h2>',
 	) );
 }
+
 add_action( 'widgets_init', 'new_york_bakery_widgets_init' );
 
 /**
@@ -118,15 +120,16 @@ add_action( 'widgets_init', 'new_york_bakery_widgets_init' );
  */
 function new_york_bakery_scripts() {
 	wp_enqueue_style( 'new-york-bakery-style', get_stylesheet_uri() );
-
+	
 	wp_enqueue_script( 'new-york-bakery-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
+	
 	wp_enqueue_script( 'new-york-bakery-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
+	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
+
 add_action( 'wp_enqueue_scripts', 'new_york_bakery_scripts' );
 
 /**
