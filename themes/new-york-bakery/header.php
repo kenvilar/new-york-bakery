5b21e76897f29edaeb2bc8ac552933385264e810
Ken Vilar <kenvilar@gmail.com>
2017-08-22 21:24:30 +0800

    Modify banner section

--- themes/new-york-bakery/header.php
+++ themes/new-york-bakery/header.php
@@ -84,15 +84,25 @@
 
 <div class="new-york-bakery-container-fluid">
 
-	<div class="banner-section">
+	<div class="banner-section"
+	     style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/images/landing-page-banner-img.png);">
 		<div class="container-fluid">
-			<div class="row">
-				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-					asdasd
-				</div>
-				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-					asdasd
+			<div class="row top-banner-one-column">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<h1><strong>Quality is is our Priority #1</strong></h1>
 				</div>
 			</div>
+			<!--<div class="row top-banner-two-columns">
+				<div class="col-lg-1 col-md-2"><h3></h3></div>
+				<div class="col-lg-5 col-md-4 col-sm-6 col-xs-12">
+					<img src="<?php /*echo get_template_directory_uri(); */ ?>/assets/images/p28-food-that-performs.png"
+					     alt="" title="" style="height:90px"/>
+				</div>
+				<div class="col-lg-5 col-md-4 col-sm-6 col-xs-12">
+					<img src="<?php /*echo get_template_directory_uri(); */ ?>/assets/images/p28-food-that-performs.png"
+					     alt="" title="" style="height:90px"/>
+				</div>
+				<div class="col-lg-1 col-md-2"><h3></h3></div>
+			</div>-->
 		</div>
 	</div>
