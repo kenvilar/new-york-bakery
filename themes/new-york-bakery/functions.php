391c601e889d8b02fdb11896c3644247f21ef697
Ken Vilar <kenvilar@gmail.com>
2017-08-22 14:59:52 +0800

    Register sidebars

--- themes/new-york-bakery/functions.php
+++ themes/new-york-bakery/functions.php
@@ -111,6 +111,33 @@ function new_york_bakery_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
+	register_sidebar( array(
+		'name'          => esc_html__( 'Footer Widget 1', 'new-york-bakery' ),
+		'id'            => 'footer-widget-1',
+		'description'   => esc_html__( 'Add footer widget here.', 'new-york-bakery' ),
+		'before_widget' => '<section id="%1$s" class="widget footer-widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+	register_sidebar( array(
+		'name'          => esc_html__( 'Footer Widget 2', 'new-york-bakery' ),
+		'id'            => 'footer-widget-2',
+		'description'   => esc_html__( 'Add footer widget here.', 'new-york-bakery' ),
+		'before_widget' => '<section id="%1$s" class="widget footer-widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+	register_sidebar( array(
+		'name'          => esc_html__( 'Footer Widget 3', 'new-york-bakery' ),
+		'id'            => 'footer-widget-3',
+		'description'   => esc_html__( 'Add footer widget here.', 'new-york-bakery' ),
+		'before_widget' => '<section id="%1$s" class="widget footer-widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
 }
 
 add_action( 'widgets_init', 'new_york_bakery_widgets_init' );
