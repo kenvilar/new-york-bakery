79b9e40ec8678fc4ede41e5bd7406972f4a6d0c6
Ken Vilar <kenvilar@gmail.com>
2017-08-24 00:34:50 +0800

    Get page css custom field

--- themes/new-york-bakery/header.php
+++ themes/new-york-bakery/header.php
@@ -17,7 +17,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	
-	<?php wp_head(); ?>
+	<?php wp_head();
+	
+	if ( get_field( 'page_css' ) ) :
+		require get_template_directory() . '/inc/custom-page-css.php';
+	endif; ?>
 </head>
 
 <body <?php body_class(); ?>>
