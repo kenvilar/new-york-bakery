ef664d3d4a9e918cfbe0f612cad8de04eeb9dd32
Ken Vilar <kenvilar@gmail.com>
2017-08-25 14:38:44 +0800

    Fix custom field rows

--- themes/new-york-bakery/front-page.php
+++ themes/new-york-bakery/front-page.php
@@ -28,75 +28,67 @@ get_header(); ?>
 		<?php endif; ?>
 
 		<div class="container">
-			<?php if ( have_rows( 'row_two_columns' ) ) : ?>
+			<?php if ( have_rows( 'row_two_columns' ) ) : while ( have_rows( 'row_two_columns' ) ) : the_row(); ?>
 
 				<div class="row">
-					
-					<?php if ( have_rows( 'first_column' ) ) : ?>
 
-						<div class="col-lg-6 col-md-6 col-md-6 col-sm-12 col-xs-12">
-							<div class="front-page-card">
-								<?php if ( get_sub_field( 'first_column_link' ) ) : ?>
-									<a href="<?php get_sub_field( 'first_column_link' ); ?>">
-										<?php if ( get_sub_field( 'first_column_background_image' ) ) : ?>
-											<div class="front-page-card-bg-img"
-											     style="background-image: url(<?php get_sub_field( 'first_column_background_image' ); ?>);
-													     height: 237px;">
-												<span class="plus-for-hover">
-													<img src="<?php echo get_template_directory_uri() . '/assets/images/plus.svg'; ?>"/>
-												</span>
-												<span class="o-for-hover" style="display: none;">
-													<img src="<?php echo get_template_directory_uri() . '/assets/images/open.png'; ?>"/>
-												</span>
-											</div>
-										<?php endif; ?>
-									</a>
-								<?php endif; ?>
-								<?php if ( get_sub_field( 'first_column_title' ) ) : ?>
-									<h4><?php get_sub_field( 'first_column_title' ); ?></h4>
-								<?php endif; ?>
-								<?php if ( get_sub_field( 'first_column_link' ) ) : ?>
-									<p><a href="<?php get_sub_field( 'first_column_link' ); ?>">learn more</a></p>
-								<?php endif; ?>
-							</div>
+					<div class="col-lg-6 col-md-6 col-md-6 col-sm-12 col-xs-12">
+						<div class="front-page-card">
+							<?php if ( get_sub_field( 'first_column_link' ) ) : ?>
+								<a href="<?php the_sub_field( 'first_column_link' ); ?>">
+									<?php if ( get_sub_field( 'first_column_background_image' ) ) : ?>
+										<div class="front-page-card-bg-img"
+										     style="background-image: url(<?php the_sub_field( 'first_column_background_image' ); ?>);
+												     height: 237px;">
+											<span class="plus-for-hover">
+												<img src="<?php echo get_template_directory_uri() . '/assets/images/plus.svg'; ?>"/>
+											</span>
+											<span class="o-for-hover" style="display: none;">
+												<img src="<?php echo get_template_directory_uri() . '/assets/images/open.png'; ?>"/>
+											</span>
+										</div>
+									<?php endif; ?>
+								</a>
+							<?php endif; ?>
+							<?php if ( get_sub_field( 'first_column_title' ) ) : ?>
+								<h4><?php the_sub_field( 'first_column_title' ); ?></h4>
+							<?php endif; ?>
+							<?php if ( get_sub_field( 'first_column_link' ) ) : ?>
+								<p><a href="<?php the_sub_field( 'first_column_link' ); ?>">learn more</a></p>
+							<?php endif; ?>
 						</div>
-					
-					<?php endif; ?>
-					
-					<?php if ( have_rows( 'second_column' ) ) : ?>
+					</div>
 
-						<div class="col-lg-6 col-md-6 col-md-6 col-sm-12 col-xs-12">
-							<div class="front-page-card">
-								<?php if ( get_sub_field( 'second_column_link' ) ) : ?>
-									<a href="<?php get_sub_field( 'second_column_link' ); ?>">
-										<?php if ( get_sub_field( 'second_column_background_image' ) ) : ?>
-											<div class="front-page-card-bg-img"
-											     style="background-image: url(<?php get_sub_field( 'second_column_background_image' ); ?>);
-													     height: 237px;">
-												<span class="plus-for-hover">
-													<img src="<?php echo get_template_directory_uri() . '/assets/images/plus.svg'; ?>"/>
-												</span>
-												<span class="o-for-hover" style="display: none;">
-													<img src="<?php echo get_template_directory_uri() . '/assets/images/open.png'; ?>"/>
-												</span>
-											</div>
-										<?php endif; ?>
-									</a>
-								<?php endif; ?>
-								<?php if ( get_sub_field( 'second_column_title' ) ) : ?>
-									<h4><?php get_sub_field( 'second_column_title' ); ?></h4>
-								<?php endif; ?>
-								<?php if ( get_sub_field( 'second_column_link' ) ) : ?>
-									<p><a href="<?php get_sub_field( 'second_column_link' ); ?>">learn more</a></p>
-								<?php endif; ?>
-							</div>
+					<div class="col-lg-6 col-md-6 col-md-6 col-sm-12 col-xs-12">
+						<div class="front-page-card">
+							<?php if ( get_sub_field( 'second_column_link' ) ) : ?>
+								<a href="<?php the_sub_field( 'second_column_link' ); ?>">
+									<?php if ( get_sub_field( 'second_column_background_image' ) ) : ?>
+										<div class="front-page-card-bg-img"
+										     style="background-image: url(<?php the_sub_field( 'second_column_background_image' ); ?>);
+												     height: 237px;">
+											<span class="plus-for-hover">
+												<img src="<?php echo get_template_directory_uri() . '/assets/images/plus.svg'; ?>"/>
+											</span>
+											<span class="o-for-hover" style="display: none;">
+												<img src="<?php echo get_template_directory_uri() . '/assets/images/open.png'; ?>"/>
+											</span>
+										</div>
+									<?php endif; ?>
+								</a>
+							<?php endif; ?>
+							<?php if ( get_sub_field( 'second_column_title' ) ) : ?>
+								<h4><?php the_sub_field( 'second_column_title' ); ?></h4>
+							<?php endif; ?>
+							<?php if ( get_sub_field( 'second_column_link' ) ) : ?>
+								<p><a href="<?php the_sub_field( 'second_column_link' ); ?>">learn more</a></p>
+							<?php endif; ?>
 						</div>
-					
-					<?php endif; ?>
+					</div>
 
 				</div>
 			
-			<?php endif; ?>
+			<?php endwhile; endif; ?>
 			<!--<div class="row">
 				<div class="col-lg-6 col-md-6 col-md-6 col-sm-12 col-xs-12">
 					<div class="front-page-card">
