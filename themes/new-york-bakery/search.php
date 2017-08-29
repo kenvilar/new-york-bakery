ce807b87c61a54df1e79be4fc6f59eb08bfda9fa
Ken Vilar <kenvilar@gmail.com>
2017-08-29 19:36:51 +0800

    Customize 404 and search template styles

--- themes/new-york-bakery/search.php
+++ themes/new-york-bakery/search.php
@@ -2,49 +2,50 @@
 /**
  * The template for displaying search results pages
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
+ * @link    https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
  *
  * @package New_York_Bakery
  */
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<div class="new-york-bakery-container-fluid">
+		<div class="container-fluid main-content">
+			<div class="white-bg">
+				
+				<?php
+				if ( have_posts() ) : ?>
 
-		<?php
-		if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title"><?php
-					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'new-york-bakery' ), '<span>' . get_search_query() . '</span>' );
-				?></h1>
-			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
-	</section><!-- #primary -->
+					<header class="page-header">
+						<h1 class="page-title"><?php
+							/* translators: %s: search query. */
+							printf( esc_html__( 'Search Results for: %s', 'new-york-bakery' ), '<span>' . get_search_query() . '</span>' );
+							?></h1>
+					</header><!-- .page-header -->
+					
+					<?php
+					/* Start the Loop */
+					while ( have_posts() ) : the_post();
+						
+						/**
+						 * Run the loop for the search to output the results.
+						 * If you want to overload this in a child theme then include a file
+						 * called content-search.php and that will be used instead.
+						 */
+						get_template_part( 'template-parts/content', 'search' );
+					
+					endwhile;
+					
+					the_posts_navigation();
+				
+				else :
+					
+					get_template_part( 'template-parts/content', 'none' );
+				
+				endif; ?>
+			</div>
+		</div><!-- #main -->
+	</div><!-- #primary -->
 
 <?php
 get_sidebar();
