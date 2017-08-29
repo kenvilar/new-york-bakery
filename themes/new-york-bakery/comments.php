3a0534537bc1794a60dfe4467cfbf7c4c9e8795f
Ken Vilar <kenvilar@gmail.com>
2017-08-29 19:43:45 +0800

    Customize single post

--- themes/new-york-bakery/comments.php
+++ themes/new-york-bakery/comments.php
@@ -5,7 +5,7 @@
  * This is the template that displays the area of the page that contains both the current comments
  * and the comment form.
  *
- * @link https://codex.wordpress.org/Template_Hierarchy
+ * @link    https://codex.wordpress.org/Template_Hierarchy
  *
  * @package New_York_Bakery
  */
@@ -21,7 +21,7 @@ if ( post_password_required() ) {
 ?>
 
 <div id="comments" class="comments-area">
-
+	
 	<?php
 	// You can start editing here -- including this comment!
 	if ( have_comments() ) : ?>
@@ -30,13 +30,13 @@ if ( post_password_required() ) {
 			$comment_count = get_comments_number();
 			if ( 1 === $comment_count ) {
 				printf(
-					/* translators: 1: title. */
+				/* translators: 1: title. */
 					esc_html_e( 'One thought on &ldquo;%1$s&rdquo;', 'new-york-bakery' ),
 					'<span>' . get_the_title() . '</span>'
 				);
 			} else {
 				printf( // WPCS: XSS OK.
-					/* translators: 1: comment count number, 2: title. */
+				/* translators: 1: comment count number, 2: title. */
 					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'new-york-bakery' ) ),
 					number_format_i18n( $comment_count ),
 					'<span>' . get_the_title() . '</span>'
@@ -44,28 +44,28 @@ if ( post_password_required() ) {
 			}
 			?>
 		</h2><!-- .comments-title -->
-
+		
 		<?php the_comments_navigation(); ?>
 
 		<ol class="comment-list">
 			<?php
-				wp_list_comments( array(
-					'style'      => 'ol',
-					'short_ping' => true,
-				) );
+			wp_list_comments( array(
+				'style'      => 'ol',
+				'short_ping' => true,
+			) );
 			?>
 		</ol><!-- .comment-list -->
-
+		
 		<?php the_comments_navigation();
-
+		
 		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if ( ! comments_open() ) : ?>
 			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'new-york-bakery' ); ?></p>
-		<?php
+			<?php
 		endif;
-
+	
 	endif; // Check for have_comments().
-
+	
 	comment_form();
 	?>
 
--- themes/new-york-bakery/single.php
+++ themes/new-york-bakery/single.php
@@ -2,33 +2,35 @@
 /**
  * The template for displaying all single posts
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ * @link    https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
  * @package New_York_Bakery
  */
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<div class="new-york-bakery-container-fluid">
+		<div class="container-fluid main-content">
+			<div class="white-bg">
+				
+				<?php
+				while ( have_posts() ) : the_post();
+					
+					get_template_part( 'template-parts/content', get_post_type() );
+					
+					the_post_navigation();
+					
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+				
+				endwhile; // End of the loop.
+				?>
 
-		<?php
-		while ( have_posts() ) : the_post();
-
-			get_template_part( 'template-parts/content', get_post_type() );
-
-			the_post_navigation();
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			</div>
+		</div>
+	</div>
 
 <?php
 get_sidebar();
