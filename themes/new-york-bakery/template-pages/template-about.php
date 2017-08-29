24850d1df6d1d7a34556fca12f6eac0aaf121437
Ken Vilar <kenvilar@gmail.com>
2017-08-29 14:02:41 +0800

    Make the hardcode timeline content to custom fields

--- themes/new-york-bakery/template-pages/template-about.php
+++ themes/new-york-bakery/template-pages/template-about.php
@@ -10,9 +10,7 @@ get_header(); ?>
 
 	<script>
 		$( function() {
-			$().timelinr( {
-				autoPlayDirection : 'forward'
-			} );
+			$().timelinr( { autoPlayDirection : 'forward' } );
 		} );
 	</script>
 
@@ -23,83 +21,42 @@ endwhile; endif;
 ?>
 
 	<div class="gray-bg timeline-history" style="padding: 22px 0 25px;">
-		<h4 class="timeline-header">OUR HISTORY</h4>
-		<div id="timeline">
-			<ul id="dates">
-				<li><a href="#1920" style="opacity:0;">1920</a></li>
-				<li><a href="#1930" style="opacity:0;">1930</a></li>
-				<li><a href="#1944" style="opacity:0;">1944</a></li>
-				<li><a href="#1950" style="opacity:0;">1950</a></li>
-				<!--<li><a href="#1971">1971</a></li>-->
-				<!--<li><a href="#1977">1977</a></li>
-				<li><a href="#1989">1989</a></li>
-				<li><a href="#1999">1999</a></li>
-				<li><a href="#2001">2001</a></li>
-				<li><a href="#2011">2011</a></li>-->
-			</ul>
-			<ul id="issues">
-				<li id="1920">
-					<h1>1920</h1>
-					<p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam
-						pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu
-						velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed
-						turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non
-						vel leo. Sed fringilla porta ligula.</p>
-				</li>
-				<li id="1930">
-					<h1>1930</h1>
-					<p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam
-						pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu
-						velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed
-						turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non
-						vel leo. Sed fringilla porta ligula.</p>
-				</li>
-				<li id="1944">
-					<h1>1944</h1>
-					<p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam
-						pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu
-						velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed
-						turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non
-						vel leo. Sed fringilla porta ligula.</p>
-				</li>
-				<li id="1950">
-					<h1>1950</h1>
-					<p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam
-						pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu
-						velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed
-						turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non
-						vel leo. Sed fringilla porta ligula.</p>
-				</li>
-				<!--<li id="1971">
-					<h1>1971</h1>
-					<p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
-				</li>-->
-				<!--<li id="1977">
-					<h1>1977</h1>
-					<p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
-				</li>
-				<li id="1989">
-					<h1>1989</h1>
-					<p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
-				</li>
-				<li id="1999">
-					<h1>1999</h1>
-					<p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
-				</li>
-				<li id="2001">
-					<h1>2001</h1>
-					<p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
-				</li>
-				<li id="2011">
-					<h1>2011</h1>
-					<p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
-				</li>-->
-			</ul>
-			<!--<div id="grad_left"></div>
-			<div id="grad_right"></div>-->
-			<!--<a href="#" id="next">+</a>
-			<a href="#" id="prev">-</a>-->
-		</div>
+		
+		<?php if ( have_rows( 'timeline_slider' ) ) : ?>
+			<h4 class="timeline-header">OUR HISTORY</h4>
+			<div id="timeline">
+				<ul id="dates">
+					<?php while ( have_rows( 'timeline_slider' ) ) : the_row(); ?>
+						
+						<?php if ( get_sub_field( 'year' ) ) : ?>
+							<li><a href="#<?php the_sub_field( 'year' ); ?>" style="opacity:0;">
+									<?php the_sub_field( 'year' ); ?></a>
+							</li>
+						<?php endif; ?>
+					<?php endwhile; ?>
+				</ul>
+				<ul id="issues">
+					<?php while ( have_rows( 'timeline_slider' ) ) : the_row(); ?>
+						<li id="<?php the_sub_field( 'year' ); ?>">
+							
+							<?php if ( get_sub_field( 'year' ) ) : ?>
+								<h1><?php the_sub_field( 'year' ); ?></h1>
+							<?php endif; ?>
+							
+							<?php if ( get_sub_field( 'timeline_content' ) ) : ?>
+								<p><?php the_sub_field( 'timeline_content' ); ?></p>
+							<?php endif; ?>
+
+						</li>
+					<?php endwhile; ?>
+				</ul>
+				<!--<div id="grad_left"></div>
+				<div id="grad_right"></div>-->
+				<!--<a href="#" id="next">+</a>
+				<a href="#" id="prev">-</a>-->
+			</div>
+		<?php endif; ?>
+
 	</div>
 
 <?php
