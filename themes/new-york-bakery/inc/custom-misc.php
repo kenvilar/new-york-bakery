bc10b2b2c9fdcac4286017b8c5172d91bb2e6bc3
Ken Vilar <kenvilar@gmail.com>
2017-09-02 01:58:18 +0800

    Add custom logo in login page

--- themes/new-york-bakery/inc/custom-misc.php
+++ themes/new-york-bakery/inc/custom-misc.php
@@ -2,3 +2,23 @@
 
 /* disable the wpautop */
 remove_filter( 'the_content', 'wpautop' );
+
+/* Change WordPress Logo on Signup Page */
+function my_login_logo_one() {
+	$custom_logo_id = get_theme_mod( 'custom_logo' );
+	$image          = wp_get_attachment_image_src( $custom_logo_id, 'full' );
+	?>
+	<style type="text/css">
+		body.login div#login h1 a {
+			background-image: url(<?php echo $image[0] . '?ver=' . get_bloginfo( 'version' ); ?>);
+			padding-bottom: 100px;
+			width: 100% !important;
+			height: auto;
+			background-size: contain;
+			background-position: center center;
+		}
+	</style>
+	<?php
+}
+
+add_action( 'login_enqueue_scripts', 'my_login_logo_one' );
