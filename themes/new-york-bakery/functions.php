a57d66510384da9933a765f3a0b0e8ae0ecd5917
Ken Vilar <kenvilar@gmail.com>
2017-09-02 02:23:34 +0800

    Revert to null for custom css and js

--- themes/new-york-bakery/functions.php
+++ themes/new-york-bakery/functions.php
@@ -152,7 +152,7 @@ function new_york_bakery_styles() {
 		array(), get_bloginfo( 'version' ), 'all' );
 	wp_enqueue_style( 'custom-css',
 		get_template_directory_uri() . '/assets/css/custom.css',
-		array(), get_bloginfo( 'version' ), 'all' );
+		array(), null, 'all' );
 }
 
 add_action( 'wp_enqueue_scripts', 'new_york_bakery_styles' );
@@ -182,7 +182,7 @@ function new_york_bakery_scripts() {
 	
 	wp_enqueue_script( 'custom-js',
 		get_template_directory_uri() . '/assets/js/custom.js',
-		array( 'jquery' ), get_bloginfo( 'version' ), true );
+		array( 'jquery' ), null, true );
 	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
