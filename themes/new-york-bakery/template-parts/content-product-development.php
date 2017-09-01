89d4c411ad35f557e4c92394f7bbeb45823a95d0
Ken Vilar <kenvilar@gmail.com>
2017-09-01 17:49:55 +0800

    Add new front-page cards for mobile

--- themes/new-york-bakery/template-parts/content-product-development.php
+++ themes/new-york-bakery/template-parts/content-product-development.php
@@ -5,6 +5,7 @@
 ?>
 
 <?php if ( have_rows( 'product_development_cards' ) ) : while ( have_rows( 'product_development_cards' ) ) : the_row(); ?>
+	<!--Desktop-->
 	<div class="row custom-row-margin-bottom product-development-cards">
 		
 		<?php if ( get_sub_field( 'first_column_card_image' ) && get_sub_field( 'first_column_card_title' ) ) : ?>
@@ -43,6 +44,64 @@
 				<?php the_sub_field( 'second_column_card_title' ); ?>
 				
 				<?php the_sub_field( 'second_column_card_content' ); ?>
+
+			</div>
+		
+		<?php endif; ?>
+
+	</div>
+
+	<!--Mobile-->
+	<div class="row custom-row-margin-bottom product-development-cards mobile">
+		
+		<?php if ( get_sub_field( 'first_column_card_image' ) && get_sub_field( 'first_column_card_title' ) ) : ?>
+
+			<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 product-development-single-card">
+				<?php if ( get_sub_field( 'first_column_icon' ) ) : ?>
+					<span class="product-development-icon step size-48">
+						<i class="icon <?php the_sub_field( 'first_column_icon' ); ?>"></i>
+					</span>
+				<?php endif; ?>
+
+				<span class="first-column-card-title"><?php the_sub_field( 'first_column_card_title' ); ?></span>
+				
+				<div class="product-development-toggle-show-content">plus</div>
+
+				<div class="product-development-toggle-content" style="display: none;">
+					<div class="product-development-card-image"
+					     style="background-image: url(<?php the_sub_field( 'first_column_card_image' ); ?>)">
+					</div>
+					
+					<?php the_sub_field( 'first_column_card_content' ); ?>
+					
+					<div class="product-development-toggle-hide-content">minus</div>
+				</div>
+
+			</div>
+		<?php endif; ?>
+		
+		<?php if ( get_sub_field( 'second_column_card_image' ) && get_sub_field( 'second_column_card_title' ) ) : ?>
+
+			<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 product-development-single-card">
+				<?php if ( get_sub_field( 'second_column_icon' ) ) : ?>
+					<span class="product-development-icon step size-48">
+							<i class="icon <?php the_sub_field( 'second_column_icon' ); ?>"></i>
+						</span>
+				<?php endif; ?>
+
+				<span class="first-column-card-title"><?php the_sub_field( 'second_column_card_title' ); ?></span>
+
+				<div class="product-development-toggle-show-content">plus</div>
+
+				<div class="product-development-toggle-content" style="display: none;">
+					<div class="product-development-card-image"
+					     style="background-image: url(<?php the_sub_field( 'second_column_card_image' ); ?>)">
+					</div>
+					
+					<?php the_sub_field( 'second_column_card_content' ); ?>
+
+					<div class="product-development-toggle-hide-content">minus</div>
+				</div>
 
 			</div>
 		
