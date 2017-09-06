5786cd21265e93e262073be188ece6dbcc03630d
Ken Vilar <kenvilar@gmail.com>
2017-09-07 01:18:42 +0800

    Fix constructor camelcase; remove unused variable

--- themes/new-york-bakery/assets/js/smoothScroll.js
+++ themes/new-york-bakery/assets/js/smoothScroll.js
@@ -7,65 +7,58 @@
  * https://github.com/maab16/smoothScroll
  */
 
-;(function($,window,document,undefined){
+;(function( $, window, document, undefined ) {
 	'use strict';
 
 	var deafults = {
+		animationType : 'ease',
+		speed         : 600,
+		stepAmount    : 300,
+		type          : 'wheel'
+	};
 
-					animationType 		: 'ease',
-					speed 				: 600,
-					stepAmount 			: 300,
-					type 				: 'wheel'
-
-				}
-
-	function smoothScroll(element,options){
-
-		this.settings = $.extend({},deafults,options);
-		this.element  = $(element);
+	function SmoothScroll( element, options ) {
+		this.settings = $.extend( {}, deafults, options );
+		this.element = $( element );
 		this.init();
+	}
 
-		};
+	SmoothScroll.prototype = {
 
-	smoothScroll.prototype = {
-
-		init : function(){
-			if ('wheel' == this.settings.type) {
+		init  : function() {
+			if ( 'wheel' === this.settings.type ) {
 				this.wheel();
 			}
 		},
-		wheel : function(){
+		wheel : function() {
 			var self = this;
 			//Credits http://stackoverflow.com/questions/26700853/smooth-scrolling-on-mouse-wheel
-			$(window).on('wheel DOMMouseScroll', function(e) {
-				self.direction;
+			$( window ).on( 'wheel DOMMouseScroll', function( e ) {
+				//self.direction;
 				self.stepAmount = self.settings.stepAmount;
 
-				  	e.preventDefault();
-				  	if(e.type === 'wheel') {
-				    	self.direction = e.originalEvent.deltaY < 0 ? '-=' : '+=';
-				  	}else {
-				    	self.direction = e.originalEvent.detail < 0 ? '-=' : '+=';
-				  	}
+				e.preventDefault();
+				if ( e.type === 'wheel' ) {
+					self.direction = e.originalEvent.deltaY < 0 ? '-=' : '+=';
+				} else {
+					self.direction = e.originalEvent.detail < 0 ? '-=' : '+=';
+				}
 
-				  self.element.stop().animate({
-				    scrollTop: self.direction + self.stepAmount
-				  },self.settings.speed);
-			});
+				self.element.stop().animate( {
+					scrollTop : self.direction + self.stepAmount
+				}, self.settings.speed );
+			} );
 		}
 
 	};
 
-	$.fn.smoothScroll = function(options){
-
-		return this.each(function(){
-
-			if (!$.data(this,'smoothScroll')) {
-
-				$.data(this,'smoothScroll',new smoothScroll(this,options));
+	$.fn.smoothScroll = function( options ) {
+		return this.each( function() {
+			if ( !$.data( this, 'smoothScroll' ) ) {
+				$.data( this, 'smoothScroll', new SmoothScroll( this, options ) );
 			}
-		});
+		} );
 	};
 
 
-})(jQuery,window,document);
+})( jQuery, window, document );
