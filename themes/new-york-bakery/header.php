8bec2dbe831439e3d59ea80928509ef58e726aaf
Ken Vilar <kenvilar@gmail.com>
2017-11-02 18:56:19 +0800

    Change home bg image with video

--- themes/new-york-bakery/header.php
+++ themes/new-york-bakery/header.php
@@ -39,66 +39,72 @@ header( $expStr );*/
 
 <div class="new-york-bakery-container-fluid">
 	
-	<?php if ( get_field( 'top_banner_section' ) == 'top-banner-one-column' ||
-	           get_field( 'top_banner_section' ) == 'top-banner-two-columns' ) : ?>
+	<?php if ( is_home() || is_front_page() ) :
+		putRevSlider( 'home_video_slider', 'homepage' );
+	else : ?>
+		<?php if ( get_field( 'top_banner_section' ) == 'top-banner-one-column' ||
+		           get_field( 'top_banner_section' ) == 'top-banner-two-columns' ) : ?>
 
-		<div class="banner-section"
-		     style="<?php
-		     if ( has_post_thumbnail( $post->ID ) ) : ?>
-				     background-image: url(<?php echo the_post_thumbnail_url(); ?>);
-		     <?php else : ?>
-				     background-image: url(<?php echo get_template_directory_uri(); ?>/assets/images/landing-page-banner-img.jpg);
-		     <?php endif; ?>">
+			<div class="banner-section"
+			     style="<?php
+			     if ( has_post_thumbnail( $post->ID ) ) : ?>
+					     background-image: url(<?php echo the_post_thumbnail_url(); ?>);
+			     <?php else : ?>
+					     background-image: url(<?php echo get_template_directory_uri(); ?>/assets/images/landing-page-banner-img.jpg);
+			     <?php endif; ?>">
 
-			<div class="container-fluid">
-				<?php if ( get_field( 'top_banner_section' ) == 'top-banner-one-column' ) : ?>
-					<div class="row top-banner-one-column">
-						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-							<?php if ( get_field( 'top_banner_one_column_content' ) ) :
-								the_field( 'top_banner_one_column_content' );
-							else : ?>
-								<?php the_title( '<h1><strong>', '</strong></h1>' ); ?>
-							<?php endif; ?>
+				<div class="container-fluid">
+					<?php if ( get_field( 'top_banner_section' ) == 'top-banner-one-column' ) : ?>
+						<div class="row top-banner-one-column">
+							<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+								<?php if ( get_field( 'top_banner_one_column_content' ) ) :
+									the_field( 'top_banner_one_column_content' );
+								else : ?>
+									<?php the_title( '<h1><strong>', '</strong></h1>' ); ?>
+								<?php endif; ?>
+							</div>
 						</div>
-					</div>
-				<?php elseif ( get_field( 'top_banner_section' ) == 'top-banner-two-columns' ) : ?>
-					<div class="row top-banner-two-columns">
-						
-						<?php if ( get_field( 'top_banner_two_columns_second_content' ) ) : ?>
+					<?php elseif ( get_field( 'top_banner_section' ) == 'top-banner-two-columns' ) : ?>
+						<div class="row top-banner-two-columns">
+							
+							<?php if ( get_field( 'top_banner_two_columns_second_content' ) ) : ?>
+
+								<div class="col-lg-1 col-md-2"><h3></h3></div>
+								<div class="col-lg-5 col-md-4 col-sm-6 col-xs-12">
+									<?php if ( get_field( 'top_banner_two_columns_first_content' ) ) :
+										the_field( 'top_banner_two_columns_first_content' );
+									else :
+										the_title( '<h1 class="entry-title">', '</h1>' );
+									endif; ?>
+								</div>
+								<div class="col-lg-5 col-md-4 col-sm-6 col-xs-12">
+									<?php the_field( 'top_banner_two_columns_second_content' ); ?>
+								</div>
+							
+							<?php else : ?>
+
+								<div class="col-lg-1"><h3></h3></div>
+								<div class="col-lg-10 col-md-10 col-sm-12 col-xs-12">
+									<?php if ( get_field( 'top_banner_two_columns_first_content' ) ) :
+										the_field( 'top_banner_two_columns_first_content' );
+									else :
+										the_title( '<h1 class="entry-title">', '</h1>' );
+									endif; ?>
+								</div>
+							
+							<?php endif; ?>
 
 							<div class="col-lg-1 col-md-2"><h3></h3></div>
-							<div class="col-lg-5 col-md-4 col-sm-6 col-xs-12">
-								<?php if ( get_field( 'top_banner_two_columns_first_content' ) ) :
-									the_field( 'top_banner_two_columns_first_content' );
-								else :
-									the_title( '<h1 class="entry-title">', '</h1>' );
-								endif; ?>
-							</div>
-							<div class="col-lg-5 col-md-4 col-sm-6 col-xs-12">
-								<?php the_field( 'top_banner_two_columns_second_content' ); ?>
-							</div>
-						
-						<?php else : ?>
 
-							<div class="col-lg-1"><h3></h3></div>
-							<div class="col-lg-10 col-md-10 col-sm-12 col-xs-12">
-								<?php if ( get_field( 'top_banner_two_columns_first_content' ) ) :
-									the_field( 'top_banner_two_columns_first_content' );
-								else :
-									the_title( '<h1 class="entry-title">', '</h1>' );
-								endif; ?>
-							</div>
-						
-						<?php endif; ?>
+						</div>
+					<?php endif; ?>
+				</div>
 
-						<div class="col-lg-1 col-md-2"><h3></h3></div>
-
-					</div>
-				<?php endif; ?>
 			</div>
-
-		</div>
-	<?php elseif ( get_field( 'top_banner_section' ) == 'no-top-banner' ) : endif; ?>
+		<?php elseif ( get_field( 'top_banner_section' ) == 'no-top-banner' ) :
+			echo '';
+		endif; ?>
+	<?php endif; ?>
 
 	<div class="container-fluid main-content">
 	
